2711d1986a5d27432139080d7595655f94596367
Ali Karami <karamiali19@gmail.com>
2018-08-01 19:51:37 +0100

    added step defs

---
+++ features/step_definitions/calc_features_step_defs.rb
@@ -0,0 +1,200 @@
+Given("I am on the calculator app") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click a number") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click add") do
+  pending # Write code here that turns the phrase above into concrete actions
+
+end
+
+When("I click another number") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click equals") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive adding two numbers answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click subtract") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the subtracting two numbers answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click divide") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the dividing two numbers answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click multiply") do
+
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the multiplying two numbers answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click sqrt button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click close bracket") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the sqrt of the numbers") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click shift button") do
+
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the cube root of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click square button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the square of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the cube of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+When("I click sin button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the sin of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the arc sin of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click hyp button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the sin hyperbolic of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+When("I click cos button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the cos of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the arc cos of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the cos hyperbolic of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click tan button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+Then("I should receive the tan of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the arc tan of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the tan hyperbolic of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click y^x button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive y to the x of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click {int} button") do |int|
+
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive factorial of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click Pi") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive Pi multiplied to a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+When("I click log button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the log of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive ten to the power of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click ln button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive the ln of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+Then("I should receive e to the power of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click percent button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive percent of a number answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive one divided by x answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
